4c31b8faa62b6c959545f9ad71283ee8cbc0f041
D4m0n <d4m0n.8@gmail.com>
2023-03-08 10:40:19 +0900

    Lab lazy: Lazy allocation + Eliminate allocation from sbrk()

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -46,9 +46,14 @@ sys_sbrk(void)
 
   if(argint(0, &n) < 0)
     return -1;
+  /*
   addr = myproc()->sz;
   if(growproc(n) < 0)
     return -1;
+*/
+
+  addr = myproc()->sz += n;
+
   return addr;
 }
 
